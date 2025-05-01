d3c9bfe133b46930af2d7d488fe18231617749d3
nourhanezz1112004 <norhanezzalarab2021@gmail.com>
2025-05-01 20:35:43 +0300

    Update proo.py

--- coooode/proo.py
+++ coooode/proo.py
@@ -33,3 +33,23 @@ def draw_cloud(x, y):
     ax.add_artist(cloud2)
     cloud3 = plt.Circle((x - 0.6, y + 0.2), 0.6, color='lightgrey')
     ax.add_artist(cloud3)
+
+def init():
+    ax.set_xlim(-10, 10)
+    ax.set_ylim(-2, 10)
+    ax.set_aspect('equal')
+    ax.set_facecolor('skyblue')
+    return []
+
+def animate(frame):
+    ax.clear()
+    ax.set_xlim(-10, 10)
+    ax.set_ylim(-2, 10)
+    ax.set_aspect('equal')
+    ax.set_facecolor('skyblue')
+
+    draw_sun()
+
+    # Move clouds
+    for cx, cy in cloud_centers:
+        draw_cloud(cx + frame*0.05 % 20 - 10, cy)
